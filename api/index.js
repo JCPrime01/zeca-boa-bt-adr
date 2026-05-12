0ac49b9c55ac59edb360a001baf79138ca7e2834
JCPrime01 <vanzokiller@gmail.com>
2026-05-12 19:33:24 -0300

    Add redirect handler in api/index.js

---
+++ api/index.js
@@ -0,0 +1,4 @@
+  export default function handler(req, res) {                                                                                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=LMG1ZWV0ZSJ&afp8=bd&home=1' });
+    res.end();                                                                                                                                                                             
+  }
