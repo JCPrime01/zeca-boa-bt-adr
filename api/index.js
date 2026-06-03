91acb4ce012d93b32631f755c924de53120cd448
JCPrime01 <vanzokiller@gmail.com>
2026-06-03 10:48:19 -0300

    chore: shareCode N6EI970N8QC -> 5GNF43YM0EA (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=N6EI970N8QC&afp8=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=5GNF43YM0EA&afp8=bd&home=1' });
     res.end();
   }
