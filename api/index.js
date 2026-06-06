1338420cf95823c5f823f02b80bfb6a323ffcdf2
JCPrime01 <vanzokiller@gmail.com>
2026-06-06 09:50:25 -0300

    chore: shareCode BEVK9K9DLRY -> J2AXQS7BXO8 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=BEVK9K9DLRY&afp8=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=J2AXQS7BXO8&afp8=bd&home=1' });
     res.end();
   }
