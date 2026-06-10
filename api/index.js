1ad3d82b001822a2710f32ed5cafa3a94cdae9e5
JCPrime01 <vanzokiller@gmail.com>
2026-06-09 21:12:05 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425?shareCode=IBE3H87O2RI&afp2=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2498f714?shareCode=IBE3H87O2RI&afp2=bd&home=1' });
     res.end();
   }
